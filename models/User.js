15da8301704906e23b0b5afa6608c3ee5a3a5fd1
krishna8206 <>
2025-05-28 14:01:15 +0530

    phone added

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: false },
   email: { type: String, unique: true, required: true },
+  phone: { type: Number, unique: true, required: true },
   role: { type: String, enum: ['User', 'Driver', 'Admin'], default: 'User' },
   vehicleType: String,
   vehicleNumber: String,
